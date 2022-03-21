273858f46b1047c39a4c30c41bbc15228ffc361c
4v3ngR <101933870+4v3ngR@users.noreply.github.com>
2022-03-21 13:59:22 +0800

    Update adblock.js

--- www/js/plugins/adblock.js
+++ www/js/plugins/adblock.js
@@ -4,9 +4,6 @@
   window.adblock = "loaded";
   console.log("loading adblock");
 
-  // TODO: find a self contained youtube music ad blocker that is MIT license compatible
-  // the one I had previously was GPL3
-
   function interceptXHR(url, text) {
     if (url.includes('youtubei/v1/player')) try {
       var obj = JSON.parse(text);
