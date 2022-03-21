d8269e43f6165a88c7382b12f0be09004f97a310
4v3ngR <avenger@rescueteam.com>
2022-03-21 17:43:15 +0800

    remove upsell button in toolbar

--- www/js/plugins/ui.js
+++ www/js/plugins/ui.js
@@ -1,4 +1,9 @@
 (function() {
+  var upsell = document.querySelector('ytmusic-pivot-bar-item-renderer[tab-id="SPunlimited"]');
+  if (upsell) {
+    upsell.setAttribute("style", "visibility: hidden !important; display: none !important;");
+  }
+
   if (window.uichanges === "loaded") return;
   window.uichanges = "loaded";
 
